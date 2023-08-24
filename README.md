185c8563d35b4a20fc0e90ef08b0a72c4f758852
Emmanuel Niyongabo <eniyongabo@miu.edu>
2023-08-24 17:19:27 -0500

    file added

---
+++ README.md
@@ -0,0 +1,2 @@
+# cs425-swe-202306
+Demo code for CS425-Software Engineering for June 2023
---
+++ librarywebapp/src/main/java/edu/miu/cs/cs425/fairfieldlibraryapp/Lesson10WebappFairfieldlibrarywebappApplication.java
@@ -0,0 +1,14 @@
+package edu.miu.cs.cs425.fairfieldlibraryapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lesson10WebappFairfieldlibrarywebappApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Hello, FairfieldLibrary webapp...");
+		SpringApplication.run(Lesson10WebappFairfieldlibrarywebappApplication.class, args);
+	}
+
+}
